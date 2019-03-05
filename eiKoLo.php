a2f01963ba181bd388c08580a7cc31e7aedef534
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2019-03-05 22:52:05 +0100

    Convert to extension registration (#6)
    * Convert to extension registration - retain old entry point
    * Convert to extension registration - create new entry point
    * Update de.json
    * Update en.json
    * Update qqq.json
    * Update COPYING

--- eiKoLo.php
+++ eiKoLo.php
@@ -7,25 +7,22 @@
  * mailto:kontakt@eikolo.net
  * https://www.eikolo.net/
  *
- * Copyright (C) 2010 - 2018
+ * Copyright (C) 2010 - 2019
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. (C)
  */
 
-// Display extension properties on MediaWiki.
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'Zusammenstellung „eiKoLo“',
-	'author' => array(
-		'Karsten Hoffmeyer'
-	),
-	'url' => 'https://www.eikolo.net/',
-	'descriptionmsg' => 'eikolo-desc',
-	'version' => '6.0.1',
-	'license-name' => 'C'
-);
-
-// Register extension messages and other localisation.
-$wgMessagesDirs['EiKoLo'] = __DIR__ . '/i18n';
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'EiKoLo' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['EiKoLo'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for EiKoLo extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return true;
+} else {
+	die( 'This version of the EiKoLo extension requires MediaWiki 1.25+' );
+}
